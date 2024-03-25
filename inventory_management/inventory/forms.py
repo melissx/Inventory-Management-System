e78e8e3c395c7bf36d355daaec6eebe389754590
Melissa <141769451+melissx@users.noreply.github.com>
2024-03-25 12:16:50 +0200

    Update forms.py

--- inventory_management/inventory/forms.py
+++ inventory_management/inventory/forms.py
@@ -1,1 +1,12 @@
+from django import forms
+from .models import Product, Stock
 
+class ProductForm(forms.ModelForm):
+  class Meta:
+    model = Product
+    fields = ['name', 'description', 'category', 'unit_price']
+
+class StockForm(forms.ModelForm):
+  class Meta:
+    model = Stock
+    fields = ['product', 'quantity', 'reorder_level']
